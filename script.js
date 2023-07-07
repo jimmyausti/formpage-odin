eec134b170af3348805bcf5c1b7ba7a0389b7e14
Jimmy <j.austin.ozu@gmail.com>
2023-07-07 15:37:18 +1200

    implement theme toggle and adjust sizing behaviour
    implement a theme switch between light and dark using a button. simplify
    website design into a simple, neo-brutalist look. Adjusted resizing
    behaviour on the html, body and main tags as well.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log('hello world!');
+
+//confirm password
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 
@@ -17,3 +19,12 @@ document.querySelector('form').addEventListener('submit', function(e) {
 
 });
 
+//toggle theme
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
+
+document.querySelector('#toggle-theme').addEventListener('click', setTheme);
